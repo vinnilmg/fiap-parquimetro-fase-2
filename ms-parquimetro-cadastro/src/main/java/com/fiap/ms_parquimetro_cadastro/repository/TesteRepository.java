ed01d18724db9b8e21e0a590096e588c8560bf17
lobaoff <gabrielflobao@hotmail.com>
2024-07-05 19:33:54 -0300

    [FEATURE] - Adicionado arquivos de configuração

---
+++ ms-parquimetro-cadastro/src/main/java/com/fiap/ms_parquimetro_cadastro/repository/TesteRepository.java
@@ -0,0 +1,4 @@
+package com.fiap.ms_parquimetro_cadastro.repository;
+
+public class TesteRepository {
+}
---
+++ ms-parquimetro-cadastro/src/main/java/com/fiap/ms_parquimetro_cadastro/service/ServiceTest.java
@@ -0,0 +1,4 @@
+package com.fiap.ms_parquimetro_cadastro.service;
+
+public class ServiceTest {
+}
