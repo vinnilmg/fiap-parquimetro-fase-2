5e40c39ebd2f74aaff1b482b6e60a1076f0735cb
vinnilmg <vini.delimagomes@gmail.com>
2024-07-07 20:01:48 -0300

    feat: Endpoint de entrada (variavel)

--- ms-parquimetro-control/src/main/java/com/fiap/ms_parquimetro_control/repository/MongoRepositoryTest.java
+++
@@ -1,9 +0,0 @@
-package com.fiap.ms_parquimetro_control.repository;
-
-import com.fiap.ms_parquimetro_control.repository.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MongoRepositoryTest extends MongoRepository<User, Long> {
-}
