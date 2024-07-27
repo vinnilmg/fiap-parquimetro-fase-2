c370e9f78b803be432827a890f68e64a84cb0ceb
vinnilmg <vini.delimagomes@gmail.com>
2024-07-27 12:48:59 -0300

    feat: Docker compose completo + correcoes

--- ms-parquimetro-cadastro/src/main/java/com/fiap/ms_parquimetro_cadastro/config/exception/GlobalExceptionHandler.java
+++ ms-parquimetro-cadastro/src/main/java/com/fiap/ms_parquimetro_cadastro/config/exception/GlobalExceptionHandler.java
@@ -4,34 +4,41 @@ import com.fiap.ms_parquimetro_cadastro.exception.carro.CarroNotFoundException;
 import com.fiap.ms_parquimetro_cadastro.exception.carro.CarroPlacaNotFoundException;
 import com.fiap.ms_parquimetro_cadastro.exception.carro.PlacaJaUtilizadaException;
 import com.fiap.ms_parquimetro_cadastro.exception.cliente.ClienteNotFoundException;
+import com.fiap.ms_parquimetro_cadastro.exception.cliente.CnhJaUtilizadaException;
 import com.fiap.ms_parquimetro_cadastro.exception.cliente.UUIDClienteInvalidException;
-import com.fiap.ms_parquimetro_cadastro.exception.generic.CustomNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({CarroNotFoundException.class, ClienteNotFoundException.class, CarroPlacaNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<CustomErrorResponse> handleNotFoundException(CustomNotFoundException ex) {
-        CustomErrorResponse response = new CustomErrorResponse(ex.getErrorCode(), ex.getMessage());
+    public ResponseEntity<CustomErrorResponse> handleNotFoundException(Exception ex) {
+        CustomErrorResponse response = new CustomErrorResponse(HttpStatus.NOT_FOUND.getReasonPhrase(), ex.getMessage());
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({
             PlacaJaUtilizadaException.class,
-            UUIDClienteInvalidException.class
+            UUIDClienteInvalidException.class,
+            CnhJaUtilizadaException.class,
     })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<CustomErrorResponse> handleBadRequestException(CustomNotFoundException ex) {
-        CustomErrorResponse response = new CustomErrorResponse(ex.getErrorCode(), ex.getMessage());
+    public ResponseEntity<CustomErrorResponse> handleBadRequestException(Exception e) {
+        CustomErrorResponse response = new CustomErrorResponse(HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
-
-
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CustomErrorResponse> handleInternalServerError(
+            final Exception e, final WebRequest request
+    ) {
+        final var response = new CustomErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), e.getMessage());
+        return ResponseEntity.internalServerError().body(response);
+    }
 }
--- ms-parquimetro-cadastro/src/main/java/com/fiap/ms_parquimetro_cadastro/controller/mapper/ClienteMapper.java
+++ ms-parquimetro-cadastro/src/main/java/com/fiap/ms_parquimetro_cadastro/controller/mapper/ClienteMapper.java
@@ -4,17 +4,11 @@ import com.fiap.ms_parquimetro_cadastro.controller.response.ClienteResponse;
 import com.fiap.ms_parquimetro_cadastro.controller.resquest.ClienteRequest;
 import com.fiap.ms_parquimetro_cadastro.repository.entity.Cliente;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface ClienteMapper {
 
     Cliente ClienteRequestToEntity(ClienteRequest request);
 
-    ClienteRequest ClienteEntityToRequest(Cliente entity);
-
     ClienteResponse ClienteEntityToResponse(Cliente cliente);
-
-    Cliente ClienteResponseToEntity(ClienteResponse response);
 }
