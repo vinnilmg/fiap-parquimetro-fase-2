fa2b0c9dae01203561f64a334e589dcfa5e4a8ac
vinnilmg <vini.delimagomes@gmail.com>
2024-07-29 19:55:11 -0300

    fix

--- ms-parquimetro-cadastro/src/main/java/com/fiap/ms_parquimetro_cadastro/controller/mapper/CarroMapper.java
+++ ms-parquimetro-cadastro/src/main/java/com/fiap/ms_parquimetro_cadastro/controller/mapper/CarroMapper.java
@@ -5,28 +5,14 @@ import com.fiap.ms_parquimetro_cadastro.controller.resquest.CarroRequest;
 import com.fiap.ms_parquimetro_cadastro.repository.entity.Carro;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface CarroMapper {
 
-    @Mappings({
-            @Mapping(source = "clienteId", target = "cliente.id")
-    })
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "clienteId", target = "cliente.id")
     Carro CarroRequestToEntity(CarroRequest request);
 
-    @Mappings({
-            @Mapping(source = "cliente.id", target = "clienteId")
-    })
-    CarroRequest CarroEntityToRequest(Carro entity);
-
-    @Mappings({
-            @Mapping(source = "cliente.id", target = "clienteId")
-    })
+    @Mapping(source = "cliente.id", target = "clienteId")
     CarroResponse CarroEntityToResponse(Carro carro);
-
-    @Mappings({
-            @Mapping(source = "clienteId", target = "cliente.id")
-    })
-    Carro CarroResponseToEntity(CarroResponse response);
 }
--- ms-parquimetro-cadastro/src/main/java/com/fiap/ms_parquimetro_cadastro/controller/mapper/ClienteMapper.java
+++ ms-parquimetro-cadastro/src/main/java/com/fiap/ms_parquimetro_cadastro/controller/mapper/ClienteMapper.java
@@ -4,10 +4,12 @@ import com.fiap.ms_parquimetro_cadastro.controller.response.ClienteResponse;
 import com.fiap.ms_parquimetro_cadastro.controller.resquest.ClienteRequest;
 import com.fiap.ms_parquimetro_cadastro.repository.entity.Cliente;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ClienteMapper {
 
+    @Mapping(target = "id", ignore = true)
     Cliente ClienteRequestToEntity(ClienteRequest request);
 
     ClienteResponse ClienteEntityToResponse(Cliente cliente);
